ae017765e5f7190018dc1ec99dcf862ca5226439
PiyushSonale <piyushsonale@gmail.com>
2025-06-07 22:45:58 +0530

    learned mailers and about css and javascript i.e. hotwire

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   root "products#index"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+
+
+
+
 end
